3e38a731af14c606446bac304550e3f59e1dbfa2
rahiji9 <aliahmedrahi007@gmail.com>
2024-12-22 22:40:24 +0600

    cors add

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://cap-store-client.vercel.app"],
     // optionsSuccessStatus: 200,
   })
 );
